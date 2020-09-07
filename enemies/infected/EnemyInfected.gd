8c5a3c33caa05c7bd8f9dd1edbda3363e6ae5079
NicolasLFerreira <54772785+NicolasLFerreira@users.noreply.github.com>
2020-09-08 09:22:38 +1200

    Started working on path finder

--- enemies/infected/EnemyInfected.gd
+++ enemies/infected/EnemyInfected.gd
@@ -4,6 +4,8 @@ var movement_speed = 150;
 var vector = Vector2();
 var gravity = 10;
 
+var body_ = preload("res://player/PlayerBodyPhysics.tscn");
+
 func _physics_process(delta):
 	
 	vector = move_and_slide(vector, Vector2(0, -1));
@@ -11,9 +13,9 @@ func _physics_process(delta):
 	if (!is_on_floor()):
 		vector.y += gravity;
 	
-	movement();
-	
 
-func movement():
-	
-	vector.x = movement_speed;
+func _finder_entered(body):
+	pass;
+
+func _finder_exited(body):
+	pass;
