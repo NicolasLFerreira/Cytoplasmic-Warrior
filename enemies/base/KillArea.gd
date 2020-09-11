211832cc7a0c8945f690b85ab3c4b786d8fccc4d
NicolasLFerreira <54772785+NicolasLFerreira@users.noreply.github.com>
2020-09-11 12:47:23 +1200

    Fixed enemies kill area

--- enemies/base/KillArea.gd
+++ enemies/base/KillArea.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 func _kill_area(body):
 	
-	if (body.get_name() == "PlayerBodyPhysics"):
+	if (body.get_name() == "PlayerBodyPhysics" and body.die):
 		body.queue_free();
 		get_tree().reload_current_scene();
 	
