7dff3f6eb0106a83970c5f0a1b7bc4988e4aa6a2
NicolasLFerreira <54772785+NicolasLFerreira@users.noreply.github.com>
2020-09-14 15:07:22 +1200

    Added buttons and title screen

---
+++ gui/menu_screens/TitleScreenMenu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func _on_PlayButton_pressed():
+	pass
+
+func _on_SettingsButton_pressed():
+	pass
+
+func _on_TutorialButton_pressed():
+	pass
+
+func _on_QuitButton_pressed():
+	get_tree().quit();
