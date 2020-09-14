cafc858807c5ffd65b8a2c3880206d1d903268d1
NicolasLFerreira <54772785+NicolasLFerreira@users.noreply.github.com>
2020-09-15 09:52:50 +1200

    Added skin textures

--- objects/map_objects/Checkpoint.gd
+++ objects/map_objects/Checkpoint.gd
@@ -2,4 +2,4 @@ extends Area2D
 
 func _on_player_entered(body):
 	if (body.get_name() == "PlayerBodyPhysics"):
-		get_cp.spawn_node = self.get_name();
+		global_script.spawn_node = self.get_name();
--- objects/textures/objects/GetCheckpoint.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-var spawn_node = "Spawn";
