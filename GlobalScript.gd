260756aef953ccef60315dbee94d403f33878eec
NicolasLFerreira <54772785+NicolasLFerreira@users.noreply.github.com>
2020-09-15 09:53:18 +1200

    Added cheatmode condition

---
+++ GlobalScript.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var cheatmode = false;
+var music = true;
+var spawn_node = "Spawn";
--- maps/MapBase.gd
+++ maps/MapBase.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
+var music = global_script.music;
 var player_pos = Vector2();
-onready var spawn_node = get_cp.spawn_node;
+onready var spawn_node = global_script.spawn_node;
 
 func _ready():
 	$PlayerBodyPhysics.position = get_node("Checkpoints").get_node(spawn_node).position;
+
+func _process(delta):
+	if (music):
+		if (!$Music.playing):
+			$Music.play();
