b241ed35463afea550f708ea5c99216aee0e0295
NicolasLFerreira <54772785+NicolasLFerreira@users.noreply.github.com>
2020-09-15 22:11:52 +1200

    Finished \'Level1\'

--- maps/MapBase.gd
+++ maps/MapBase.gd
@@ -1,12 +1,10 @@
 extends Node2D
 
 var player_pos = Vector2();
-onready var spawn_node = global_script.spawn_node;
 
 func _ready():
-	$PlayerBodyPhysics.position = get_node("Checkpoints").get_node(spawn_node).position;
+	$PlayerBodyPhysics.position = get_node("Checkpoints").get_node(global_script.spawn_node).position;
 	global_script.enemies = $Enemies.get_child_count();
-	global_script.spawn_node = "Spawn";
 
 func _process(delta):
 	if (global_script.music and !$Music.playing):
