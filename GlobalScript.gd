1e875ba76f03f11864bf0468a6d197b1755589b0
NicolasLFerreira <54772785+NicolasLFerreira@users.noreply.github.com>
2020-09-15 21:08:46 +1200

    Added crystal, finished tutorial and added a few global variables

--- GlobalScript.gd
+++ GlobalScript.gd
@@ -1,5 +1,6 @@
 extends Node
 
+var enemies = 1;
 var cheatmode = false;
 var music = true;
 var spawn_node = "Spawn";
--- maps/MapBase.gd
+++ maps/MapBase.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
-var music = global_script.music;
 var player_pos = Vector2();
 onready var spawn_node = global_script.spawn_node;
 
 func _ready():
 	$PlayerBodyPhysics.position = get_node("Checkpoints").get_node(spawn_node).position;
+	global_script.enemies = $Enemies.get_child_count();
+	global_script.spawn_node = "Spawn";
 
 func _process(delta):
-	if (music):
-		if (!$Music.playing):
-			$Music.play();
+	if (global_script.music and !$Music.playing):
+		$Music.play();
